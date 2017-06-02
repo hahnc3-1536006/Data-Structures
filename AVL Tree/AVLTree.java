704d12563c6a5e7c299d1a1a8d78716b86742592
hahnc3-1536006 <hahnc3@uw.edu>
2017-06-02 00:11:35 -0700

    avl updates

--- AVL Tree/AVLTree.java
+++ AVL Tree/AVLTree.java
@@ -11,16 +11,15 @@ import java.util.*;
 public class AVLTree implements StringTree{
 	
 	private class AVLNode{
-		//Do not change these variable names
+
 		String key;
 		String value;
 		AVLNode left;
 		AVLNode right;
 		
-		//Place any additional fields you need here
       int height;
 			
-		//TODO implement the node class here
+		// constructs an AVL node with key, value and height attributes
       private AVLNode(String key, String val, int height, AVLNode left, AVLNode right) {
          this.key = key;
          this.value = val;
@@ -28,7 +27,8 @@ public class AVLTree implements StringTree{
          this.left = left;
          this.right = right;
       }
-
+      
+      // constructs a leaf AVL node
       private AVLNode(String key, String val, int height) {
          this(key, val, height, null, null);
       }     
@@ -37,30 +37,34 @@ public class AVLTree implements StringTree{
 	//Use this variable as your root
 	private AVLNode root;
 	private int size;
-	//You may use any additional fields here as you see fit
    
+   // constructs an empty AVL tree
    public AVLTree() {
       this.root = null;
       this.size = 0;
    }
 	
+   // removes all elements in an AVL tree
 	public void makeEmpty() {
-		// TODO Remove all elements from the AVL tree.
       this.root = null;
+      this.size = 0;
 	}
 	
+   // returns the number of elements currently in the tree
 	public int size() {
-		// TODO Return the number of elements currently in the tree.
 		return this.size;
 	}
    
+   // inserts a key, value pair into the AVL tree using the self-balancing
+   // AVL property
+   // throws IllegalArgumentException if a duplicate key is inserted
    public void insert(String key, String value) {
       root = insert(key, value, root);
    }
-
+   
+   // helper method returning an AVL node and performing the rotations 
+   // neccessary after a node is inserted into the tree
 	private AVLNode insert(String key, String value, AVLNode root) {
-		// TODO Insert the <key,value> pair into the AVLTree
-		// Throw an IllegalArgumentException if the client attempts to insert a duplicate key
       if (root == null) {
          root = new AVLNode(key, value, 0);
          size++;
@@ -73,16 +77,20 @@ public class AVLTree implements StringTree{
       }
       root.height = Math.max(height(root.left), height(root.right)) + 1;
       int balanceNum = balance(root);
+      // left left case
       if (balanceNum > 1 && key.compareTo(root.left.key) < 0) {
          root = rightRotation(root);
       } 
+      // left right case
       if (balanceNum > 1 && key.compareTo(root.left.key) > 0) { 
          root.left = leftRotation(root.left);
          root = rightRotation(root);
       }
+      // right right case
       if (balanceNum < -1 && key.compareTo(root.right.key) > 0) {
          root = leftRotation(root);
       }
+      // right left case
       if (balanceNum < -1 && key.compareTo(root.right.key) < 0) {
          root.right = rightRotation(root.right);
          root = leftRotation(root);
@@ -90,6 +98,7 @@ public class AVLTree implements StringTree{
       return root;
 	}
    
+   // returns the height of a single node
    private int height(AVLNode node) {
       if (node == null) {
          return 0;
@@ -97,6 +106,7 @@ public class AVLTree implements StringTree{
       return node.height;
    }
    
+   // returns the difference in heights of a node's left and right subtrees
    private int balance(AVLNode node) {
       if (node == null) {
          return 0;
@@ -104,6 +114,7 @@ public class AVLTree implements StringTree{
       return height(node.left) - height(node.right);
    }
    
+   // performs a left rotation
    private AVLNode leftRotation(AVLNode r) {
       AVLNode pivot = r.right;
       r.right = pivot.left;
@@ -115,6 +126,7 @@ public class AVLTree implements StringTree{
       return pivot;
    }
    
+   // performs a right rotation
    private AVLNode rightRotation(AVLNode l) {
       AVLNode pivot = l.left;
       l.left = pivot.right;
@@ -126,9 +138,9 @@ public class AVLTree implements StringTree{
       return pivot;
    }   
    
+   // returns the value affiliated with the String key
+   // throws an ObjectNotFoundException if the key is not in the AVLTree
 	public String find(String key) {
-		// TODO Return the value affiliated with the String key.
-		// Throw an ObjectNotFoundException if the key is not in the AVLTree
       AVLNode current = root;
       while (current.key != key) {
          if (current.key.compareTo(key) < 0) {
@@ -142,30 +154,63 @@ public class AVLTree implements StringTree{
       }
 	   return current.value;
 	}
-
+   
+   // returns a BFSIterator 
 	public Iterator<String> getBFSIterator() {
-		// TODO Only complete this section if you wish to attempt the 10 points EC
-		// This function should return a BFSIterator: Starter code provided below
-		return null;
+      BFSIterator iterator = new BFSIterator(root);
+		return iterator;
 	}
 	
-/*	Define your private Iterator class below.
-
+   // BFSIterator class for Strings with functionality for returning the 
+   // next key of a node in a BFS node or a boolean if the next key exists
 	private class BFSIterator implements Iterator<String>{
+   
+      private Queue<AVLNode> queue; 
+      private Queue<String> queue2;
+      
+      // constructs a BFSIterator 
+      public BFSIterator(AVLNode root) {
+         if (root != null) {
+            queue = new LinkedList<AVLNode>();
+            queue2 = new LinkedList<String>();
+            queue.add(root);
+         }
+         while (!queue.isEmpty()) {
+            AVLNode n = queue.remove();
+            queue2.add(n.key);
+            if (n.left != null) {
+               queue.add(n.left);
+            }
+            if (n.right != null) {
+               queue.add(n.right);
+            }
+         }
+      }
 		
+      // returns true if the iterator has another value to return
 		public boolean hasNext() {
-			// TODO Return true if the iterator has another value to return
-			return false;
+			return !queue2.isEmpty();
 		}
-
+      
+      // returns the next value in a BFS traversal of the tree
+      // starts at the root and iterates through left children before right
 		public String next() {
-			// TODO Return the next value in a BFS traversal of the tree
-			// It should start at the root and iterate through left children before right
-			return null;
-		}
-	
+// 			if (hasNext()) {
+//             while (!queue.isEmpty()) {
+//                AVLNode node = queue.remove();
+//                if (node.left != null) {
+//                   queue.add(node.left);
+//                } 
+//                if (node.right != null) {
+//                   queue.add(node.right);
+//                }
+//                return node.value;
+//             }
+//          }
+         if (hasNext()) {
+            return queue2.remove();
+         }
+         return null;
+		}   
 	}
-*/
-
-
 }
